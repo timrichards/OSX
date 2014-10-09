fabc89545c1ba162c70352f8e00ca7351a8dcaa1
Tim Richards <tim.richards.dev@gmail.com>
2014-10-08 22:51:49 -0400

    let structs are immutable...

--- SlotMachine/SlotMachine/ViewController.swift
+++ SlotMachine/SlotMachine/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
     
     private func createViews()
     {
+        1 + 1                                                               // no lvalue builds just fine
+
         let heights:[CGFloat] = [1, 3, 1, 1]
         let colors:[UIColor] = [UIColor.redColor(), UIColor.blackColor(), UIColor.lightGrayColor(), UIColor.blackColor()]
 
@@ -37,13 +39,38 @@ class ViewController: UIViewController {
             self.view.addSubview(view)
         }
         
-        // First view.
+        // First view: Title
         self.titleLabel.text = "Super Slots"                                // A        mutable let object
         self.titleLabel.textColor = UIColor.yellowColor()
         self.titleLabel.font = UIFont(name: "MarkerFelt-Wide", size: 40)
         self.titleLabel.sizeToFit()
         self.titleLabel.center = views[0].center
         views[0].addSubview(self.titleLabel)
+        
+        // Second view: largest: Slots
+        let kNumCols = 3
+        let kNumSlots = 3
+        let kColMargin:CGFloat = 4
+        let kSlotMargin:CGFloat = 2
+        let secondView = views[1]
+        
+        for var ixCol = 0; ixCol < kNumCols; ++ixCol
+        {
+            for var ixSlot = 0; ixSlot < kNumSlots; ++ixSlot
+            {
+                var slotImageView = UIImageView()
+                slotImageView.backgroundColor = UIColor.yellowColor()
+                slotImageView.frame.origin.x = secondView.bounds.origin.x + secondView.bounds.size.width * CGFloat(ixCol)/CGFloat(kNumCols)
+                slotImageView.frame.origin.y = secondView.bounds.origin.y + secondView.bounds.size.height * CGFloat(ixSlot)/CGFloat(kNumSlots)
+                slotImageView.frame.size.width = secondView.bounds.width/CGFloat(kNumCols) - kColMargin
+                slotImageView.frame.size.height = secondView.bounds.height/CGFloat(kNumSlots) - kSlotMargin
+                
+                // even the overall border, if not removing it
+                slotImageView.frame.origin.x += kColMargin/2
+                slotImageView.frame.origin.y += kSlotMargin/2
+                secondView.addSubview(slotImageView)
+            }
+        }
     }
     
     override func viewDidLoad() {
